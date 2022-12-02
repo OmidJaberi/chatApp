1c3f849fde0db1f2178915fa11c48943bb9ab448
Captain Code <61006078+OmidJaberi@users.noreply.github.com>
2022-12-02 04:10:32 +0330

    authentication added

--- chat-client/main.js
+++ chat-client/main.js
@@ -1,28 +1,94 @@
 import './style.css';
 
-const socket = io('http://localhost:3000', { transports : ['websocket'] });
+let socket;
+let token;
+const username = document.getElementById('user');
+const password = document.getElementById('pass');
+const newUsername = document.getElementById('new-user');
+const newPassword = document.getElementById('new-pass');
+const signupContainer = document.getElementById('signup-container');
+const loginContainer = document.getElementById('login-container');
 const senderContainer = document.getElementById('send-container');
 const newMessage = document.getElementById('message-input');
 const messageContainer = document.getElementById('message-container');
 
-function addMessage(message, sender='other') {
-  const tag = `<div>
-    <div class="message${sender == 'me' ? ' me' : ''}">${message}</div>
+function addMessage(message, sender) {
+  const tag = `${sender != token ?  sender + ':' : ''} <div>
+    <div class="message${sender == token ? ' me' : ''}">${message}</div>
   </div>`;
   messageContainer.innerHTML += tag;
   window.scrollTo(0,document.body.scrollHeight);
 }
 
-socket.on('message-event', data => {
-  addMessage(data);
-});
+signupContainer.onsubmit = async e => {
+  e.preventDefault();
+  fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: newUsername.value,
+      password: newPassword.value
+    })
+  })
+    .then(res =>  {
+      if (res.status === 201) {
+        alert('User created');
+        document.getElementById('signupcover').style.display = 'none';
+      }
+      else {
+        alert(`Error status: ${res.status}.`);
+      }
+    })
+    .catch(err => console.log(err));
+}
+
+
+loginContainer.onsubmit = async e => {
+  e.preventDefault();
+  fetch('http://localhost:3000/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: username.value,
+      password: password.value
+    })
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.msg === 'Success') {
+        token = data.token;
+        document.getElementById('cover').style.display = 'none';
+        socket = io('http://localhost:3000', { transports : ['websocket'] });
+        socket.emit('add-user', token);
+        socket.on('message-event', data => {
+          addMessage(data.message, data.from);
+        });
+      }
+      else {
+        alert(data.msg);
+      }
+    })
+    .catch(err => console.log(err));
+}
 
 senderContainer.onsubmit = e => {
   e.preventDefault();
   const message = newMessage.value;
   if (message != '') {
-    addMessage(message, 'me');
-    socket.emit('send-message', message);
+    addMessage(message, token);
+    socket.emit('send-message', {
+      'token': token,
+      'message': message
+    });
   }
   newMessage.value = '';
+}
+
+document.getElementById('showSignup').onclick = () => {
+  const cover = document.getElementById('signupcover');
+  cover.style.display = 'block';
 }
--- chat-server/index.js
+++ chat-server/index.js
@@ -1,13 +1,79 @@
-const io = require("socket.io")(3000);
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const bcrypt = require('bcrypt');
 
-users = 0;
+app.use(cors());
+app.use(express.json());
+
+let usercount = 0;
+const users = [];
+const online = {};
+
+
 
 io.on('connection', socket => {
-	console.log(`New User: ${users}`);
-	users++;
 	//socket.emit('message-event', 'Hello There!');
-	socket.on('send-message', message => {
-		console.log(`New Message: ${message}`);
-		socket.broadcast.emit('message-event', message);
+	socket.on('add-user', token => {
+		online[this] = token;
+		console.log(`New User: ${usercount} - ${token}`);
+		usercount++;
+	});
+	socket.on('send-message', data => {
+		console.log(`New Message: ${data.message} from ${data.token}`);
+		socket.broadcast.emit('message-event', {
+			'from': data.token,
+			'message': data.message
+		});
 	});
 });
+
+server.listen(3000, () => {
+	console.log('listening on *:3000');
+});
+
+
+app.get('/users', (req, res) => {
+  res.json(users);
+})
+
+app.post('/users', async (req, res) => {
+	console.log(`new user: ${ req.body.name}`)
+	try {
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+		const user = { name: req.body.name, password: hashedPassword };
+		users.push(user);
+		res.status(201).send();
+	} catch {
+		res.status(500).send();
+	}
+})
+  
+
+app.post('/users/login', async (req, res) => {
+	const user = users.find(user => user.name === req.body.name);
+	if (user == null) {
+		return res.status(400).send({
+			'msg': 'Cannot find user'
+		});
+	}
+	try {
+		if(await bcrypt.compare(req.body.password, user.password)) {
+			res.send({
+				'msg': 'Success',
+				'token': user.name
+			});
+		}
+		else {
+			res.send({
+				'msg': 'Password is not correct.'
+			});
+		}
+	} catch {
+		res.status(500).send()
+	}
+})
